c71f7813a5e28cca291719431ec1b041b0517fdb
ritvik1799 <77438373+ritvik1799@users.noreply.github.com>
2021-10-28 10:07:52 +0530

    Add files via upload

---
+++ dawood.c
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        int i=0,j=n-1;
+        int mid = (i+j)/2;
+        cout<<arr[mid]<<endl;
+    }
+    return 0;
+}
